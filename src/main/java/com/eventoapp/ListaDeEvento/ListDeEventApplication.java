801c3df6db099e6b20c48ab9bc7db258bf4fba3c
Wander Souza <wanderrodrigues_@outlook.com>
2021-05-08 01:38:47 -0300

    :brain: initial-commit

---
+++ src/main/java/com/eventoapp/ListaDeEvento/ListDeEventApplication.java
@@ -0,0 +1,13 @@
+package com.eventoapp.ListaDeEvento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListDeEventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListDeEventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventoapp/ListaDeEvento/ListaDeEventoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventoapp.ListaDeEvento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListaDeEventoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
